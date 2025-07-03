e536dc21fd0c16fe9fb2e3963fe6d05b9d0f3302
ailing1021 <andywei32@gmail.com>
2025-07-03 10:57:52 +0800

    Update money_net.py

--- money_net.py
+++ money_net.py
@@ -230,5 +230,8 @@ def contact():
 def terms():
     return render_template('terms.html')
     
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
